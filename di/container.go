701ceb8c54e7296c6227e4fb92953f2352d8ece2
Kyle Grunwald <kgrunwald@gmail.com>
2019-06-01 23:54:46 -0500

    Moar docs

--- di/container.go
+++ di/container.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 )
 
+// The Container interface defines methods for the container
 type Container interface {
 	Register(interface{})
 	Get(name string) interface{}
@@ -13,11 +14,14 @@ type Container interface {
 	Print()
 }
 
+// The ServiceContainer is a naive implentation of a container that stores all services as singletons
 type ServiceContainer struct {
 	Services     map[string]interface{}
 	Constructors map[string]Constructor
 }
 
+// A Constructor is a function that takes in the container as an argument, retrieves all necessary services, and returns
+// a new instance of an `interface{}`
 type Constructor func(*ServiceContainer) interface{}
 
 var svc *ServiceContainer
@@ -28,14 +32,17 @@ func init() {
 		Constructors: make(map[string]Constructor),
 	}
 
-	// add the container as a service in the container
+	// Add the container as a service in the container. Meta.
 	svc.Register(GetContainer)
 }
 
+// GetContainer returns an implementation of a `Container`
 func GetContainer() Container {
 	return svc
 }
 
+// Register adds a service to the container. The provided constructor must only take in other services as arguments,
+// and it must return 1 value to be added to the container.
 func (c *ServiceContainer) Register(ctor interface{}) {
 	ctorType := reflect.TypeOf(ctor)
 	returnType := ctorType.Out(0)
@@ -46,10 +53,13 @@ func (c *ServiceContainer) Register(ctor interface{}) {
 	}
 }
 
+// Invoke will call the provided method with the values resolved from the container. All arguments must be services that
+// are defined in the container.
 func (c *ServiceContainer) Invoke(f interface{}) {
 	c.Call(f)
 }
 
+// Call is the same as `Invoke`, but the values returned from the function are propagated back to the caller as `[]reflect.Value`.
 func (c *ServiceContainer) Call(f interface{}) []reflect.Value {
 	fType := reflect.TypeOf(f)
 	args := []reflect.Value{}
@@ -63,6 +73,7 @@ func (c *ServiceContainer) Call(f interface{}) []reflect.Value {
 	return reflect.ValueOf(f).Call(args)
 }
 
+// GetTypeName generates the name of the service entry in the container from the Type.
 func (c *ServiceContainer) GetTypeName(t reflect.Type) string {
 	if t.Kind() == reflect.Ptr {
 		t = t.Elem()
@@ -70,6 +81,7 @@ func (c *ServiceContainer) GetTypeName(t reflect.Type) string {
 	return t.String()
 }
 
+// Get a service from the container by name. If the service is not in the container, the method will panic.
 func (c *ServiceContainer) Get(name string) interface{} {
 	if _, ok := c.Services[name]; !ok {
 		if ctor, ok := c.Constructors[name]; ok {
@@ -82,16 +94,19 @@ func (c *ServiceContainer) Get(name string) interface{} {
 	return c.Services[name]
 }
 
+// GetMethod looks up the service in the container by name and then looks up the method by name on the returned instance.
 func (c *ServiceContainer) GetMethod(service, method string) reflect.Value {
 	svc := c.Get(service)
 	t := reflect.ValueOf(svc)
 	return t.MethodByName(method)
 }
 
+// GetValue returns an entry from the container as a reflect.Value
 func (c *ServiceContainer) GetValue(name string) reflect.Value {
 	return reflect.ValueOf(c.Get(name))
 }
 
+// Print prints out the contents of the container
 func (c *ServiceContainer) Print() {
 	fmt.Println(c)
 }
