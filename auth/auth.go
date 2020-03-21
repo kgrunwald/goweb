f171f17856b60cefd62dd4455a7a37fdd1efbad0
Kyle Grunwald <kgrunwald@gmail.com>
2020-03-21 18:42:23 -0500

    Add jwt

---
+++ auth/auth.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kgrunwald/goweb"
+	"github.com/kgrunwald/goweb/ctx"
+	"github.com/kgrunwald/goweb/di"
+	"github.com/kgrunwald/goweb/ilog"
+	"github.com/kgrunwald/goweb/router"
+)
+
+type Authenticator interface {
+	Authenticate(ctx.Context)
+	Middleware(next http.Handler) http.Handler
+}
+
+type ContextKey int
+
+const (
+	ContextKeyAuthenticated ContextKey = iota
+	ContextKeyJWTAuthenticated
+	ContextKeyClaims
+	ContextKeyUserEmail
+)
+
+func RequireHTTPS() {
+	di.GetContainer().Invoke(func (r router.Router, logger ilog.Logger, info *goweb.ServerInfo) {
+		r.Use(HttpsMiddleware(logger, info.Port))
+	})
+}
+
+const HEADER_FORWARDED_PROTO = "x-forwarded-proto"
+const HEADER_HOST = "host"
+
+func HttpsMiddleware(log ilog.Logger, port int) router.Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			host := r.Header.Get(HEADER_HOST)
+			localhost := fmt.Sprintf("localhost:%d", port)
+			urlScheme := r.URL.Scheme
+			headerScheme := r.Header.Get(HEADER_FORWARDED_PROTO)
+			if host != localhost && urlScheme != "https" && headerScheme != "https" {
+				log.Info("Redirecting to HTTPS")
+				r.URL.Scheme = "https"
+				url := r.URL.String()
+				http.Redirect(w, r, url, 301)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
---
+++ auth/jwt.go
@@ -0,0 +1,109 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/kgrunwald/goweb/ctx"
+	"github.com/kgrunwald/goweb/ilog"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
+)
+
+type JWTScheme struct {
+	key string
+	roles []string
+	log ilog.Logger
+}
+
+func (j *JWTScheme) Authenticate(ctx ctx.Context) error {
+	authToken, err := ctx.Request().Cookie("authorization")
+	log := ctx.Log()
+	if err != nil || len(authToken.Value) == 0 {
+		log.Info("No JWT cookie provided")
+		return errors.New("No JWT cookie provided")
+	}
+
+	tok, err := jwt.ParseSigned(string(authToken.Value))
+	if err != nil {
+		log.WithField("error", err).Error("Could not parse JWT")
+		return err
+	}
+
+	claims := jwt.Claims{}
+	if err := tok.Claims(j.key, &claims); err != nil {
+		log.WithField("error", err).Error("Could not validate signature")
+		return err
+	}
+
+	ctx.Log().WithField("claims", claims).Info("Authenticated user")
+	ctx.AddValue(ContextKeyAuthenticated, true)
+	ctx.AddValue(ContextKeyJWTAuthenticated, true)
+	ctx.AddValue(ContextKeyClaims, &claims)
+	ctx.AddValue(ContextKeyUserEmail, claims.Subject)
+	return nil
+}
+
+func (j *JWTScheme ) Middlware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		context := ctx.New(r, w, j.log)
+		if err := j.Authenticate(context); err != nil {
+			context.Forbidden(err)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+type JWTContext struct {
+	key string
+}
+
+func (j *JWTContext) UpdateJWTCookie(ctx ctx.Context) error {
+	claims := ctx.GetValue(ContextKeyClaims).(*jwt.Claims)
+	return j.SetJWTCookie(ctx, claims)
+}
+
+func (j *JWTContext) SetJWTCookie(ctx ctx.Context, claims *jwt.Claims) error {
+	sig, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.HS256, Key: j.key}, (&jose.SignerOptions{}).WithType("JWT"))
+	if err != nil {
+		return err
+	}
+
+	claims.NotBefore = jwt.NewNumericDate(time.Now().UTC())
+	raw, err := jwt.Signed(sig).Claims(claims).CompactSerialize()
+	if err != nil {
+		return err
+	}
+
+	cookie := makeCookie(ctx, raw)
+	http.SetCookie(ctx.Writer(), cookie)
+	return nil
+}
+
+func makeCookie(ctx ctx.Context, token string) *http.Cookie {
+	referer := ctx.Request().Header.Get("Referer")
+	u, _ := url.Parse(referer)
+	domain := u.Hostname()
+	if len(domain) == 0 {
+		domain = os.Getenv("COOKIE_DOMAIN")
+	}
+
+	cookie := &http.Cookie{
+		Path: "/",
+		Domain: domain,
+		Expires: time.Now().Add(30 * 24 * time.Hour),
+		Value: token,
+		HttpOnly: true,
+	}
+	return cookie
+}
+
+func DeleteJWTCookie(ctx ctx.Context) {
+	cookie := makeCookie(ctx, "")
+	cookie.Expires = time.Now()
+	http.SetCookie(ctx.Writer(), cookie)
+}
