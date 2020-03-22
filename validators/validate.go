5d547cd499f8a583a15c471a4caf03d592b2dff3
Kyle Grunwald <kgrunwald@gmail.com>
2020-03-22 12:31:47 -0500

    Add validators

---
+++ validators/validate.go
@@ -0,0 +1,22 @@
+package validators
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func ValidateRequiredFields(o interface{}, fields ...string) error {
+	v := reflect.ValueOf(o).Elem()
+	var missing []string
+	for _, field := range fields {
+		fieldVal := v.FieldByName(field)
+		if !fieldVal.IsValid() || (fieldVal.Interface() == reflect.Zero(fieldVal.Type()).Interface()) {
+			missing = append(missing, field)
+		}
+	}
+	if len(missing) > 0 {
+		return fmt.Errorf("Missing required fields %+v", missing)
+	}
+
+	return nil
+}
